4c334d3a9371a68d5cb240f7836571eac905a3bd
PIFALIa <ozhogov.98@mail.ru>
2024-11-23 15:11:41 +0600

    new

--- halpersPakage/halpers.go
+++ halpersPakage/halpers.go
@@ -7,19 +7,22 @@ import (
 
 func MarksDel(str string) string {
 
+	runesE := []rune(str)
+	if string(runesE[0]) != "\"" {
+		panic("Первый аргумент болжен быть строкой!")
+	}
 	str = strings.ReplaceAll(str, "\"", "")
 	counts := make(map[string]int)
 	slStr := strings.Fields(str)
 
 	result := "\""
-	runes := []rune(str)
+	//runes := []rune(str)
 	resultRues := ""
 
 	minus := false
 	mno := false
 	del := false
 	pluse := false
-
 	for _, s := range slStr {
 
 		switch s {
@@ -42,7 +45,10 @@ func MarksDel(str string) string {
 	if del {
 
 		runesHelp := []rune(str)
-		num, _ := strconv.Atoi(string(runesHelp[len(runes)-1]))
+		num, err := strconv.Atoi(slStr[len(slStr)-1])
+		if err != nil {
+			panic("Ошибка преобразования:" + slStr[len(slStr)-1] + "не является целым числом ") // Паника при ошибке
+		}
 		for i, s := range runesHelp {
 			if i < num {
 				result += string(s)
@@ -51,16 +57,18 @@ func MarksDel(str string) string {
 	}
 
 	if mno {
-		num, _ := strconv.Atoi(slStr[len(slStr)-1])
-
+		num, err := strconv.Atoi(slStr[len(slStr)-1])
+		if err != nil {
+			panic("Ошибка преобразования: не является целым числом " + slStr[len(slStr)-1]) // Паника при ошибке
+		}
 		if num < 1 {
-			result = "\"программа принимает значение от 1 до 10\""
-			return result
+			panic("\"программа принимает значение от 1 до 10\"")
+
 		}
 
 		if num > 10 {
-			result = "\"программа принимает значение до 10 включительно\""
-			return result
+			panic("\"программа принимает значение до 10 включительно\"")
+
 		}
 		for range num {
 			resultRues += slStr[0]
@@ -83,14 +91,14 @@ func MarksDel(str string) string {
 		}
 	}
 
-	for i, s := range counts {
+	for i := range counts {
 
 		if minus && counts[i] < 2 {
 
 			if i == "-" {
 				break
 			}
-			if slStr[s] != slStr[s+2] {
+			if slStr[0] == slStr[len(slStr)-1] {
 				result += " "
 			}
 			result += i
